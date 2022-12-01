16386bea7c385b3f66b4ba64f032345e30e967c3
Nico Martinucci <domenico.martinucci@gmail.com>
2022-12-01 12:12:00 -0800

    add question route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey as survey
+from surveys import satisfaction_survey as survey # "survey" is our survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -9,3 +9,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 RESPONSES = []
+
+@app.get("/")
+def start_survey():
+
+    return render_template(
+        "survey_start.html", 
+        title=survey.title, 
+        instructions=survey.instructions
+    )
+
+@app.get("/questions/<question_number>")
+def get_question(question_number):
+
+    # grab question_number
+    # access that question from the survey object
+    # access the choices from the survey object
+    # return a template, passing the question and options to it
+
+    # prompt = survey.questions[question_number].prompt
+    # choices = survey.questions[question_number].choices
+
+    question = survey.questions[question_number]
+
+    return render_template("question.html", question=question)
